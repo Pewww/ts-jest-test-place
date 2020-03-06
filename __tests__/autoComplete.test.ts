ee118430786b112647f75914c26752d5223a5ec0
Your Name <you@example.com>
2020-03-06 15:52:18 +0900

    :ok_hand: 코드리뷰 반영 (각 테스트마다의 설명 디테일하게 변경)

--- __tests__/autoComplete.test.ts
+++ __tests__/autoComplete.test.ts
@@ -5,31 +5,31 @@ describe('autoComplete Start', () => {
     id: 1,
     name: '한의플래닛'
   }
-  it('get auto complete not found', () => {
+  it('존재하지 않는 아이디로 getAutoComplate을 실행 할 경우 null을 반환해야 한다. ', () => {
     expect(getAutoComplete(999)).toBeNull();
   })
 
-  it('set auto complete none pkId', () => {
+  it('존재하지 않는 아이디로 setAutoComplete을 실행 할 경우 null을 반환해야 한다.', () => {
     expect(setAutoComplete(null, data)).toBeNull();
   })
 
-  it('set auto complete none data', () => {
+  it('존재하지 않는 데이터로 setAutoComplete을 실행 할 경우 null을 반황해야 한다.', () => {
     setAutoComplete(1,null);
     
     expect(getAutoComplete(2)).toBeNull();
   })
 
-  it('set auto complete', () => {
+  it('정상적인 방법으로 setAutoComplete와 getAutoComplete을 실행 할 경우 원하던 결과 값을 반환해야 한다.', () => {
     setAutoComplete(2, data);
     
     expect(getAutoComplete(2)).toEqual(data);
   });
 
-  it('get auto complete', () => {
+  it('정상적인 방법으로 getAutoComplete을 실행할 경우 원하던 결과 값을 반환해야 한다.', () => {
     expect(getAutoComplete(2)).toEqual(data);
   })
 
-  it('clear auto complete', () => {
+  it('clearAutoComplete을 실행 후 getAutoComplete을 실행 할 경우 null을 반환해야 한다.', () => {
     clearAutoComplete();
     expect(getAutoComplete(1)).toBeNull();
   })
